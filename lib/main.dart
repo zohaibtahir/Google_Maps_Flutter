e08ed585ac410bf5c065a9ff37209b11150dd57a
Zohaib Tahir <38902366+zohaibtahir@users.noreply.github.com>
2020-10-05 15:00:09 +0500

    Adding Search Address functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:geocoder/geocoder.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -24,11 +30,67 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  GoogleMapController mapController;
+  String searchAddress;
+  final LatLng _center = const LatLng(30.3753, 69.3451);
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: Stack(
+          children: [
+            GoogleMap(
+              initialCameraPosition: CameraPosition(target: _center),
+              onMapCreated: onMapCreated,
+            ),
+            Positioned(
+              top: 50,
+                right: 15,
+                left: 15,
+                child: Container(
+                  color: Colors.white,
+                  child: Row(
+                    children: [
+                      IconButton(
+                          icon: Icon(Icons.search),
+                          onPressed: searchandNavigate),
+                      Expanded(
+                          child: TextField(
+                            cursorColor: Colors.black,
+                            keyboardType: TextInputType.text,
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              contentPadding:
+                                EdgeInsets.symmetric(horizontal: 15),
+                              hintText: "Search"
+                            ),
+                            onChanged: (val){
+                              setState(() {
+                                searchAddress = val;
+                              });
+                            },
+                          )
+                      )
+                    ],
+                  ),
+            ))
+          ],
+        )
+    );
   }
+
+  Future<Null> searchandNavigate() async{
+    await Geolocator().placemarkFromAddress(searchAddress).then((value) async{
+      await mapController.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+          target:
+          LatLng(value[0].position.latitude,value[0].position.longitude),
+          zoom:10.0)));
+    });
+  }
+
+  void onMapCreated(GoogleMapController controller) {
+    setState(() {
+      mapController = controller;
+    });
+  }
+
 }
-
-
-
